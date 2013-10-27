5c4e6df0950f4977431823036268eefa9d304cdb
David Gomez <davgomgar@gmail.com>
2013-10-27 14:25:07 +0100

    Homework 3.3 done

--- hw3-2and3/src/main/java/course/BlogPostDAO.java
+++ hw3-2and3/src/main/java/course/BlogPostDAO.java
@@ -21,7 +21,9 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
@@ -80,19 +82,18 @@ public class BlogPostDAO {
     }
 
 
-    // White space to protect the innocent
-
-
     // Append a comment to a blog post
     public void addPostComment(final String name, final String email, final String body,
                                final String permalink) {
 
-        // XXX HW 3.3, Work Here
-        // Hints:
-        // - email is optional and may come in NULL. Check for that.
-        // - best solution uses an update command to the database and a suitable
-        //   operator to append the comment on to any existing list of comments
 
+        BasicDBObject comment = new BasicDBObject("author", name).append("body", body);
+        if (!StringUtils.isEmpty(email)) {
+            comment.append("email", email);
+        }
+
+        postsCollection.update(new BasicDBObject("permalink", permalink), new BasicDBObject("$set",
+                new BasicDBObject("comments", Arrays.asList(comment))), true, false);
 
     }
 
